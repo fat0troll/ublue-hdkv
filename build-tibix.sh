4e45c43268c4c7c2719706aed621cc25f624c63b
Vladimir Hodakov <vladimir@hodakov.me>
2025-01-28 02:47:29 +0400

    Fix fsearch installation

--- build-tibix.sh
+++ build-tibix.sh
@@ -4,6 +4,12 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
+# fsearch dependency
+# may break on Fedora 42!
+# and yes, it's the official way to install it
+# see https://github.com/cboxdoerfer/fsearch/issues/584
+dnf -y install https://dl.fedoraproject.org/pub/fedora/linux/releases/40/Everything/x86_64/os/Packages/l/libicu73-73.2-1.fc40.x86_64.rpm
+
 # Install additional packages
 dnf -y install dmg2img \
     python3-vkbasalt-cli \

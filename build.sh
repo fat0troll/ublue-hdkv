d5e44887c752b077c59cb1a90ce070ee6957b71f
Vladimir Hodakov <vladimir@hodakov.me>
2025-06-04 16:08:38 +0400

    Automate docker group (based on bazzite-dx scripts)

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
+dnf5 install --enable-repo="copr:copr.fedorainfracloud.org:ublue-os:packages" -y \
+    ublue-setup-services
+
 # Install docker
 dnf5 -y install docker-ce \
         docker-ce-cli \
@@ -50,11 +53,22 @@ dnf5 -y install darkly # klassy is still broken
 # Install mingw64-gcc for Nikke
 dnf5 -y install mingw64-gcc mingw32-gcc
 
-# Enable docker socket
+# Enable services
 systemctl enable docker.socket
+systemctl enable podman.socket
+systemctl enable ublue-system-setup.service
+systemctl --global enable ublue-user-setup.service
 
 # Install ZSH configuration
 cd /usr/share
 git clone https://code.pztrn.name/hdkv/zsh-config.git
 cd zsh-config
 git checkout hdkv/ublue-hdkv
+
+# Load iptable_nat module for docker-in-docker.
+# See:
+#   - https://github.com/ublue-os/bluefin/issues/2365
+#   - https://github.com/devcontainers/features/issues/1235
+mkdir -p /etc/modules-load.d && cat >>/etc/modules-load.d/ip_tables.conf <<EOF
+iptable_nat
+EOF

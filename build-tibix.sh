37df8e7e6d5adff9d2bd76c194c090b5cdc0386a
Tibix <fuloptibi03@gmail.com>
2025-08-08 18:20:51 +0300

    feat(tibix): Add perf tool

--- build-tibix.sh
+++ build-tibix.sh
@@ -13,7 +13,8 @@ dnf5 -y install dmg2img \
     ksysguard6 \
     qdirstat \
     git-lfs \
-    binutils
+    binutils \
+    perf
 
 # TODO: follow ngrok releases from AUR
 wget --hsts-file /tmp/.wget-hsts -O /tmp/ngrok.tar.gz https://bin.equinox.io/a/9VU6NY9RyvK/ngrok-v3-3.19.1-linux-amd64.tar.gz

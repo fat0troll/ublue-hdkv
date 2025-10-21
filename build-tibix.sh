4156d796d30a46aa6e170a43d5907d2b659aaadc
Tibix <fuloptibi03@gmail.com>
2025-10-22 00:55:46 +0300

    Modify package installations in build-tibix.sh
    Updated package installations in build-tibix.sh, including adding 'virt-viewer' and updating 'amdgpu_top' to version 0.11.0.

--- build-tibix.sh
+++ build-tibix.sh
@@ -15,11 +15,8 @@ dnf5 -y install dmg2img \
     git-lfs \
     binutils \
     perf \
-    sysprof
-
-# GhosTTY
-dnf5 -y copr enable scottames/ghostty
-dnf5 -y install ghostty
+    sysprof \
+    virt-viewer
 
 # TODO: follow ngrok releases from AUR
 wget --hsts-file /tmp/.wget-hsts -O /tmp/ngrok.tar.gz https://bin.equinox.io/a/9VU6NY9RyvK/ngrok-v3-3.19.1-linux-amd64.tar.gz
@@ -31,7 +28,7 @@ rm -r /tmp/ngrok*
 # TODO: make sure these packages are also auto-updated!!
 # maybe let dependabot handle those?
 # amdgpu_top
-dnf5 -y install https://github.com/Umio-Yasuno/amdgpu_top/releases/download/v0.10.4/amdgpu_top-0.10.4-1.x86_64.rpm
+dnf5 -y install https://github.com/Umio-Yasuno/amdgpu_top/releases/download/v0.11.0/amdgpu_top-0.11.0-1.x86_64.rpm
 
 # HowToConvert
 dnf5 -y install https://static.tibix.hdkv.online/How.to.Convert-1.0.5-1.x86_64.rpm

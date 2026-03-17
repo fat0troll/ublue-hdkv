619158b07c33eb972ac589a4b821abd63b574fd7
Vladimir Hodakov <vladimir@hodakov.me>
2026-03-17 15:08:19 +0300

    Remove Konsole for hdkv

--- build-hdkv.sh
+++ build-hdkv.sh
@@ -7,8 +7,8 @@ RELEASE="$(rpm -E %fedora)"
 # Install Ghostty
 dnf5 -y install ghostty
 
-# ...and remove Ptyxis. It's nice but I prefer another terminal.
-dnf5 -y remove ptyxis
+# ...and remove Ptyxis and Konsole. It's nice but I prefer another terminal.
+dnf5 -y remove ptyxis konsole
 
 # Install vk-hdr-layer: temporary hack for Nvidia + HDR without gamescope
 dnf5 -y install vk-hdr-layer

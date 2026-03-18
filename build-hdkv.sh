389af990b30484c58fa35dbd197716db93cba071
Vladimir Hodakov <vladimir@hodakov.me>
2026-03-18 23:58:49 +0300

    Revert "Add Merkuro Calendar"
    This reverts commit 6bf0f2cef687751fb7a697ec1031b923b7213911.

--- build-hdkv.sh
+++ build-hdkv.sh
@@ -13,32 +13,8 @@ dnf5 -y remove ptyxis konsole
 # Install vk-hdr-layer: temporary hack for Nvidia + HDR without gamescope
 dnf5 -y install vk-hdr-layer
 
-# Install bazaar development dependencies
-dnf5 -y install meson \
-        cmake \
-        blueprint-compiler \
-        appstream-devel \
-        gtk4-devel \
-        libadwaita-devel \
-        flatpak-devel \
-        glycin-devel \
-        glycin-gtk4-devel \
-        libyaml-devel \
-        libsoup \
-        libsoup-devel \
-        libdex-devel \
-        libxmlb-devel \
-        json-glib-devel \
-        md4c-devel \
-        webkitgtk6.0 \
-        webkitgtk6.0-devel \
-        libsecret-devel
-
 # Dependencies for https://github.com/go-gl/glfw
-dnf5 -y install libX11-devel \
-        libXcursor-devel \
-        libXrandr-devel \
-        libXinerama-devel \
-        libXi-devel \
-        libXxf86vm-devel \
+dnf5 -y install libX11-devel libXcursor-devel \
+        libXrandr-devel libXinerama-devel \
+        libXi-devel libXxf86vm-devel \
         libglvnd-devel

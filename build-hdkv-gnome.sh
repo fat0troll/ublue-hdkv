f2074a636e0b52a6c1b9bde70e3c878d47ede0cd
Vladimir Hodakov <vladimir@hodakov.me>
2026-03-25 21:31:51 +0300

    Add GNOME variant of ublue-hdkv (because why not)

---
+++ build-hdkv-gnome.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+RELEASE="$(rpm -E %fedora)"
+
+# Install Ghostty
+dnf5 -y install ghostty
+
+# ...and remove Ptyxis. It's nice but I prefer another terminal.
+dnf5 -y remove ptyxis
+
+# Install vk-hdr-layer: temporary hack for Nvidia + HDR without gamescope
+dnf5 -y install vk-hdr-layer
+
+# Dependencies for https://github.com/go-gl/glfw
+dnf5 -y install libX11-devel libXcursor-devel \
+        libXrandr-devel libXinerama-devel \
+        libXi-devel libXxf86vm-devel \
+        libglvnd-devel
---
+++ build-shared-gnome.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+RELEASE="$(rpm -E %fedora)"
+
+dnf5 install \
+        --enable-repo="copr:copr.fedorainfracloud.org:ublue-os:packages" -y \
+        ublue-setup-services
+
+# Install docker
+dnf5 -y install docker-ce \
+        docker-ce-cli \
+        docker-buildx-plugin \
+        docker-compose-plugin \
+        containerd.io
+
+# Install zsh and utilites for it
+dnf5 -y install zsh \
+        zsh-autosuggestions
+
+# Install flatpak builder
+dnf5 -y install flatpak-builder
+
+# Install Inter font
+dnf5 -y install rsms-inter-fonts
+
+# Install goverlay
+dnf5 -y install goverlay
+
+# Install VS Code
+dnf5 -y install code
+
+# Install useful CLI tools
+dnf5 -y install telnet
+
+# Enable services
+systemctl enable docker.socket
+systemctl enable podman.socket
+systemctl enable ublue-system-setup.service
+systemctl --global enable ublue-user-setup.service
+
+# Install ZSH configuration
+cd /usr/share
+git clone https://github.com/fat0troll/zsh-config.git
+cd zsh-config
+git checkout hdkv/ublue-hdkv
+
+# Load iptable_nat module for docker-in-docker.
+# See:
+#   - https://github.com/ublue-os/bluefin/issues/2365
+#   - https://github.com/devcontainers/features/issues/1235
+mkdir -p /etc/modules-load.d && cat >>/etc/modules-load.d/ip_tables.conf <<EOF
+iptable_nat
+EOF

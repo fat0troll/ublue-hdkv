630150dcb906ac623278d2dde60b2a6c86416a58
Vladimir Hodakov <vladimir@hodakov.me>
2026-08-23 19:39:46 +0300

    You can't rm /var there

--- build_files/cleanup.sh
+++ build_files/cleanup.sh
@@ -7,9 +7,3 @@ dnf5 clean all
 
 # Clean temporary files
 rm -rf /tmp/* || true
-
-# Cleanup the entirety of `/var`.
-# None of these get in the end-user system and bootc lints get super mad if anything is in there
-rm -rf /var
-mkdir -p /var/tmp
-chmod -R 1777 /var/tmp

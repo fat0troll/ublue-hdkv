ea569c8b1a84916fbbab490f97f34ba03babc64e
Vladimir Hodakov <vladimir@hodakov.me>
2026-09-03 02:32:14 +0300

    Replace faugus-launcher with PortProton

--- build_files/build-vtsk.sh
+++ build_files/build-vtsk.sh
@@ -10,10 +10,10 @@ dnf5 -y copr disable scottames/ghostty
 # ...and remove Konsole. It's nice but I prefer another terminal.
 dnf5 -y remove konsole
 
-# Install faugus
-dnf -y copr enable faugus/faugus-launcher
-dnf -y install faugus-launcher
-dnf -y copr disable faugus/faugus-launcher
+# Install portproton
+dnf -y copr enable boria138/portproton
+dnf -y install portproton
+dnf -y copr disable boria138/portproton
 
 # ..and remove Lutris. I don't use it.
 dnf -y remove lutris

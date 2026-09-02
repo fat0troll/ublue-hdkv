c71fa8419a4d9d34676a79c347f0195fe036934e
Vladimir Hodakov <vladimir@hodakov.me>
2026-09-03 01:44:14 +0300

    Change lutris for faugus on vtsk image

--- build_files/build-vtsk.sh
+++ build_files/build-vtsk.sh
@@ -9,3 +9,11 @@ dnf5 -y copr disable scottames/ghostty
 
 # ...and remove Konsole. It's nice but I prefer another terminal.
 dnf5 -y remove konsole
+
+# Install faugus
+dnf -y copr enable faugus/faugus-launcher
+dnf -y install faugus-launcher
+dnf -y copr disable faugus/faugus-launcher
+
+# ..and remove Lutris. I don't use it.
+dnf -y remove lutris
